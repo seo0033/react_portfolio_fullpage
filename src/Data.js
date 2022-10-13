da591a95748904376deb93013dd004ae8313ff5d
seo0033 <tkrhk9511@gmail.com>
2022-10-14 08:48:42 +0900

    20221014

---
+++ src/Data.js
@@ -0,0 +1,25 @@
+const profile =
+{
+    name: "jangseoyoung",
+    email: "tkrhk9511@gmail.com",
+    tel: "010-2981-3734"
+}
+
+export { profile }
+
+const portfolio = [
+    {
+        id: 1, title: "타이틀01", type: "Responsive Web", font: ["폰트1", "폰트2", "폰트3"]
+        , info: "브랜드의 원래 로고나 제품 패키지 디자인을 \n참고하여, 모던하고 딥한 이미지를 고취시킬\n수 있는 다크모드로 디자인 했습니다.\n전체적으로 무채색을 사용하여 브랜드의 정체성을\n드러냈고, 첫번째 섹션은 브랜드만이 가지고 있는\n특이점 이라고 생각해 타이포그라피와 색감을\n이용해 디자인했습니다.", color: ["#369", "#f99", "#ff9"], skill: ["react", "react-router-dom", "axios"], src: process.env.PUBLIC_URL + "/img/pf01.jpg", link: "https://seo0033.github.io/"
+    },
+    {
+        id: 2, title: "타이틀02", type: "Responsive Web", font: ["폰트1", "폰트2", "폰트3"]
+        , info: "브랜드의 원래 로고나 제품 패키지 디자인을 \n참고하여, 모던하고 딥한 이미지를 고취시킬\n수 있는 다크모드로 디자인 했습니다.\n전체적으로 무채색을 사용하여 브랜드의 정체성을\n드러냈고, 첫번째 섹션은 브랜드만이 가지고 있는\n특이점 이라고 생각해 타이포그라피와 색감을\n이용해 디자인했습니다.", color: ["#369", "#f99", "#ff9"], skill: ["react", "react-router-dom", "axios"], src: process.env.PUBLIC_URL + "/img/pf01.jpg", link: "https://seo0033.github.io/"
+    },
+    {
+        id: 3, title: "타이틀03", type: "Responsive Web", font: ["폰트1", "폰트2", "폰트3"]
+        , info: "브랜드의 원래 로고나 제품 패키지 디자인을 \n참고하여, 모던하고 딥한 이미지를 고취시킬\n수 있는 다크모드로 디자인 했습니다.\n전체적으로 무채색을 사용하여 브랜드의 정체성을\n드러냈고, 첫번째 섹션은 브랜드만이 가지고 있는\n특이점 이라고 생각해 타이포그라피와 색감을\n이용해 디자인했습니다.", color: ["#369", "#f99", "#ff9"], skill: ["react", "react-router-dom", "axios"], src: process.env.PUBLIC_URL + "/img/pf01.jpg", link: "https://seo0033.github.io/"
+    }
+]
+
+export { portfolio }
